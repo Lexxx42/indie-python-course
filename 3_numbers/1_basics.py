4f698c8f1410037d6e8353a162a2aa1614c0f89c
Alexandr Konukhov <jesusmisha@gmail.com>
2023-01-07 19:39:08 +0300

    dzen + numbers

---
+++ 3_numbers/1_basics.py
@@ -0,0 +1,56 @@
+# Числа и операции над ними
+
+# У возведения в степень приоритет над остальными операциями. Кроме скобок.
+
+# Встроенные математические функции по работе с числами
+
+# abs() - absolute value
+# min() - minimum value
+# max() - maximum value
+# sum() - summation of values
+# mean() - mean value
+# median() - median value
+# std() - standard deviation
+# var() - variance
+# skew() - skewness
+# kurt() - kurtosis
+
+# wtf? Tabnine nuts! Very cool!
+
+# pow() - power value
+# round() - round value - по умолчанию округляет до целого числа, отбрасывая дробную часть.
+# floor() - floor value
+# ceiling() - ceiling value
+# sqrt() - square root value
+# exp() - exponential value
+# log() - natural logarithm value
+# log10() - base 10 logarithm value
+
+# bloody hell!!!!! The best!
+
+print(round(3.5))
+print(round(2.5))
+
+# Можно округлять с точностью до заданного разряда
+
+print(round(3.565, 2))
+print(round(2.565, 2))
+
+# Можно округлить целое число
+
+print(round(3565, -1))
+print(round(2565, -2))
+
+# число
+# 123.456
+# разряды
+# -2 -1 0 1 2 3
+
+print(8 % 41)  # Если второе число больше первого, то первое число и есть остаток от деления.
+
+print(16 ** 0.5)
+print(2 ** 2)
+print(2 ** 2.0)  # Если хоть одно число дробное, то ответ дробный.
+
+print(round(1/3, 2))
+print(round(1/3))
