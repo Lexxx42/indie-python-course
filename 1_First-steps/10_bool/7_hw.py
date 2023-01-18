5f2d91a8b52d6f1e8aeedc195f9298429ecc72cc
Alexandr Konukhov <jesusmisha@gmail.com>
2023-01-18 19:37:25 +0300

    7

---
+++ 1_First-steps/10_bool/7_hw.py
@@ -0,0 +1,22 @@
+# На вход поступают два слова.
+#
+# Программа должна вывести True, если хотя бы одно слово равно слову "awesome".
+#
+# Сделать задачу необходимо без использования условного оператора.
+#
+# Sample Input 1:
+#
+# cool
+# awesome
+# Sample Output 1:
+#
+# True
+# Sample Input 2:
+#
+# aaa
+# aaa
+# Sample Output 2:
+#
+# False
+
+print("awesome" in (input(), input()))
