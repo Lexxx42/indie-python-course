604da03f7c0c6cb5bf8da4a2652e9a72bc183150
Alexander Konukhov <jesusmisha@gmail.com>
2023-01-30 23:00:40 +0300

    2_hw

---
+++ 1_First-steps/11_round_up_down/1_1_test.py
@@ -0,0 +1,10 @@
+from math import floor, ceil
+
+print(floor(2.5))  # 2
+print(floor(2.2))  # 2
+print(floor(2.7))  # 2
+print(floor(2.9))  # 2
+
+print(ceil(2.5))  # 3
+print(ceil(2.9))  # 3
+print(ceil(2.2))  # 3
