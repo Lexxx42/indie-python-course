f2981561ec4af20f587d5f9e064c8ef979075ad7
Alexander Konukhov <jesusmisha@gmail.com>
2023-02-04 19:51:24 +0300

    hw10

---
+++ 2_Strings_and_lists/1_strings_and_operations/10_hw.py
@@ -0,0 +1,17 @@
+# Напишите программу, которая считывает с клавиатуры слово (или фразу, неважно),
+# после чего вывести данную строку, увеличенную по длине в 3 раза.
+
+# Sample Input 1:
+
+# Hello
+# Sample Output 1:
+
+# HelloHelloHello
+# Sample Input 2:
+
+# Python is the best
+# Sample Output 2:
+
+# Python is the bestPython is the bestPython is the best
+
+print(input() * 3)
