4c2dcaa3a0d5d1642a5fdfd06efb26fce037623a
Alexander Konukhov <jesusmisha@gmail.com>
2023-02-04 20:21:48 +0300

    hw1

---
+++ 2_Strings_and_lists/2_string_and_slices/hw_1.py
@@ -0,0 +1,16 @@
+# Программа получает на вход строку и ваша задача вывести первый элемент данной строки
+
+# Sample Input 1:
+
+# Zemfira
+# Sample Output 1:
+
+# Z
+# Sample Input 2:
+
+# It
+# Sample Output 2:
+
+# I
+
+print(a[0] if len(a := input()) >= 1 else "string index out of range")
