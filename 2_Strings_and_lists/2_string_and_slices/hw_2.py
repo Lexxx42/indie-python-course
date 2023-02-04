3c138945b5fd9f99271cd1b5ad1f480c398116a2
Alexander Konukhov <jesusmisha@gmail.com>
2023-02-04 20:23:40 +0300

    hw2

---
+++ 2_Strings_and_lists/2_string_and_slices/hw_2.py
@@ -0,0 +1,16 @@
+# Программа получает на вход строку и ваша задача вывести последний символ этой строки
+
+# Sample Input 1:
+
+# Сплин
+# Sample Output 1:
+
+# н
+# Sample Input 2:
+
+# Billie Eilish
+# Sample Output 2:
+
+# h
+
+print(a[-1] if len(a := input()) >= 1 else "string index out of range")
