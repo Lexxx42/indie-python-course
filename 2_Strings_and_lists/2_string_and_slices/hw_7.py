3079b49ff060b814bcc512b6fc3441065cf981d2
Alexander Konukhov <jesusmisha@gmail.com>
2023-02-04 20:33:13 +0300

    hw7

---
+++ 2_Strings_and_lists/2_string_and_slices/hw_7.py
@@ -0,0 +1,10 @@
+# Программа получает на вход строку. Ваша задача развернуть строку и вывести ее на экран.
+
+# Sample Input:
+
+# leetcode.com
+# Sample Output:
+
+# moc.edocteel
+
+print(input()[::-1])
