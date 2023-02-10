760aa00371ea92b2a408677b3d523b9e70f9a7f3
Alexandr Konukhov <jesusmisha@gmail.com>
2023-02-10 16:33:57 +0300

    10

---
+++ 2_Strings_and_lists/4_string_methods2/10_hw.py
@@ -0,0 +1,25 @@
+# Напишите программу, которая проверяет состоит ли введенная строка целиком из заглавных букв
+
+# В качестве ответа необходимо вывести True, если условие выполняется,
+# во всех остальных случаях нужно вывести False
+
+# Sample Input 1:
+
+# ABRACADABRA
+# Sample Output 1:
+
+# True
+# Sample Input 2:
+
+# This Good
+# Sample Output 2:
+
+# False
+# Sample Input 3:
+
+# QWeRTY
+# Sample Output 3:
+
+# False
+
+print(input().isupper())
