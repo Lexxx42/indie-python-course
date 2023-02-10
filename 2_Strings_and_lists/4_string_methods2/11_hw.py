2fe2f1613c6067be646ffc00bf71d9ebf7885107
Alexandr Konukhov <jesusmisha@gmail.com>
2023-02-10 16:35:57 +0300

    11

---
+++ 2_Strings_and_lists/4_string_methods2/11_hw.py
@@ -0,0 +1,25 @@
+# Напишите программу, которая проверяет состоит ли введенная строка целиком из строчных букв
+
+# В качестве ответа необходимо вывести True, если условие выполняется,
+# во всех остальных случаях нужно вывести False
+
+# Sample Input 1:
+
+# opportunity
+# Sample Output 1:
+
+# True
+# Sample Input 2:
+
+# Salary
+# Sample Output 2:
+
+# False
+# Sample Input 3:
+
+# cowboy
+# Sample Output 3:
+
+# True
+
+print(input().islower())
