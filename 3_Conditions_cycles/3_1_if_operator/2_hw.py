30b7ae601cff9f40388f74fcf9f7fb506ecc53e2
Alexander Konukhov <jesusmisha@gmail.com>
2023-02-24 13:04:29 +0300

    cycles

---
+++ 3_Conditions_cycles/3_1_if_operator/2_hw.py
@@ -0,0 +1,30 @@
+# На вход программе поступает одно слово.
+
+# Если это строка «Python», программа выводит ДА, в противном случае программа выводит НЕТ
+
+# Sample Input 1:
+
+# C++
+# Sample Output 1:
+
+# НЕТ
+# Sample Input 2:
+
+# Java
+# Sample Output 2:
+
+# НЕТ
+# Sample Input 3:
+
+# Python
+# Sample Output 3:
+
+# ДА
+# Sample Input 4:
+
+# python
+# Sample Output 4:
+
+# НЕТ
+
+print('ДА' if input() == 'Python' else 'НЕТ')
