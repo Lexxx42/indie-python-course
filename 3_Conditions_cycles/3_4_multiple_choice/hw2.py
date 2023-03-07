a3c313a84c0e58916184973a619c0e3323269626
Alexandr Konukhov <jesusmisha@gmail.com>
2023-03-07 11:03:40 +0300

    hw

--- 3_Conditions_cycles/3_4_multiple_choice/hw2.py
+++ 3_Conditions_cycles/3_4_multiple_choice/hw2.py
@@ -20,3 +20,14 @@
 # Sample Output 2:
 #
 # 2
+
+numbers = {}
+
+for _ in range(3):
+    if (x := int(input())) in numbers:
+        numbers[x] = numbers.get(x) + 1
+    else:
+        numbers[x] = 1
+
+print(0 if sorted(numbers.items(), key=lambda item: item[1])[-1][1] == 1 else
+      sorted(numbers.items(), key=lambda item: item[1])[-1][1])
