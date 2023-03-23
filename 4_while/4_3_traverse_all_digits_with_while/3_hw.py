ed7f7c669c93bd73da6be7534ac4924178eb8209
Alexander Konukhov <jesusmisha@gmail.com>
2023-03-23 18:42:23 +0300

    hw2

---
+++ 4_while/4_3_traverse_all_digits_with_while/3_hw.py
@@ -0,0 +1,19 @@
+# Программа принимает на вход одно натуральное число
+# и выводит на экран сумму цифр данного числа
+
+# Sample Input 1:
+
+# 123
+# Sample Output 1:
+
+# 6
+# Sample Input 2:
+
+# 891101
+# Sample Output 2:
+
+# 20
+
+# print(sum(map(int, *input().split())))
+
+print(sum(map(int, input())))
