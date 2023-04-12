ae2058681e848207a2e8f9ed90e077c2dac04087
Alexander Konukhov <jesusmisha@gmail.com>
2023-04-12 10:26:36 +0300

    hw14 KISS

--- 5_iterable_objects/3_for_lists_strings/14_hw.py
+++ 5_iterable_objects/3_for_lists_strings/14_hw.py
@@ -36,22 +36,34 @@
 
 # {()}
 
-input_string = input()
+# input_string = input()
+#
+# for _ in range(len(input_string) // 2):
+#     open_br_index = input_string.find('(')
+#     if ')' in input_string[open_br_index:] and input_string.rfind(')') > open_br_index and (input_string.rfind(
+#             ')') - input_string.find('(')) % 2:
+#         input_string = input_string.replace('(', '', 1)
+#         input_string = input_string.replace(')', '', 1)
+#     open_br_index = input_string.find('[')
+#     if ']' in input_string[open_br_index:] and input_string.rfind(']') > open_br_index and (input_string.rfind(
+#             ']') - input_string.find('[')) % 2:
+#         input_string = input_string.replace('[', '', 1)
+#         input_string = input_string.replace(']', '', 1)
+#     open_br_index = input_string.find('{')
+#     if '}' in input_string[open_br_index:] and input_string.rfind('}') > open_br_index and (input_string.rfind(
+#             '}') - input_string.find('{')) % 2:
+#         input_string = input_string.replace('{', '', 1)
+#         input_string = input_string.replace('}', '', 1)
+# print('YES' if not len(input_string) else 'NO')
 
-for _ in range(len(input_string) // 2):
-    open_br_index = input_string.find('(')
-    if ')' in input_string[open_br_index:] and input_string.rfind(')') > open_br_index and (input_string.rfind(
-            ')') - input_string.find('(')) % 2:
-        input_string = input_string.replace('(', '', 1)
-        input_string = input_string.replace(')', '', 1)
-    open_br_index = input_string.find('[')
-    if ']' in input_string[open_br_index:] and input_string.rfind(']') > open_br_index and (input_string.rfind(
-            ']') - input_string.find('[')) % 2:
-        input_string = input_string.replace('[', '', 1)
-        input_string = input_string.replace(']', '', 1)
-    open_br_index = input_string.find('{')
-    if '}' in input_string[open_br_index:] and input_string.rfind('}') > open_br_index and (input_string.rfind(
-            '}') - input_string.find('{')) % 2:
-        input_string = input_string.replace('{', '', 1)
-        input_string = input_string.replace('}', '', 1)
-print('YES' if not len(input_string) else 'NO')
+# s = input()
+# while ('()' in s) | ('{}' in s) | ('[]' in s):
+#     s = s.replace('()', '')
+#     s = s.replace('{}', '')
+#     s = s.replace('[]', '')
+# print('NO' if s else 'YES')
+
+n = input()
+while '()' in n or '{}' in n or '[]' in n:
+    n = n.replace('()', '').replace('{}', '').replace('[]', '')
+print("YES" if len(n) == 0 else "NO")
