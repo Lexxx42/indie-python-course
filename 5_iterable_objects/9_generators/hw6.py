0ebcae6adfa21a3dd1ade369c5a051b1f3eff1c3
Alexander Konukhov <jesusmisha@gmail.com>
2023-06-04 09:39:27 +0300

    hw

---
+++ 5_iterable_objects/9_generators/hw6.py
@@ -0,0 +1,4 @@
+# Создайте список первых букв каждого слова из строки st и выведите его на экран
+
+st = 'Create a list of the first letters of every word in this string'
+print([i[0] for i in st.split()])
