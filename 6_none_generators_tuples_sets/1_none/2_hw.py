db675a8c0725bcdbe5aefe2a7b8b398f5982d1e2
Alexandr Konukhov <jesusmisha@gmail.com>
2023-06-05 14:50:31 +0300

    none

---
+++ 6_none_generators_tuples_sets/1_none/2_hw.py
@@ -0,0 +1,17 @@
+# Создайте две переменные empty и empty_too, сохраните в них значение None
+#
+# При помощи оператора is выведите на первой строке результат их сравнения на равенство и затем на второй строке результат их сравнения на неравенсто
+#
+# Sample Input:
+#
+# Sample Output:
+#
+# True
+# False
+
+empty = None
+empty_too = None
+
+print(empty is empty_too)
+print(empty is not empty_too)
+
---
+++ 6_none_generators_tuples_sets/1_none/3_hw.py
@@ -0,0 +1,3 @@
+# Создайте список i_love_none из 50 элементов None и распечатайте его
+
+print(i_love_none := [None for _ in range(50)])
